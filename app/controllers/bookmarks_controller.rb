bcb0b341f0d3cba9c3f721d55cd16ca66dbc53da
Alan Perkul <alanperkul@gmail.com>
2022-05-05 18:03:11 -0300

    cod ok falta estilo

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: %i[show destroy]
+
+  def show
+  end
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  movie = Movie.new(
+    title: Faker::Movie.title.to_s,
+    overview: Faker::Lorem.sentences(number: 1).to_s,
+    poster_url: Faker::LoremFlickr.image(size: "50x60"),
+    rating: Faker::Number.decimal(l_digits: 1)
+  )
+  movie.save
+end
